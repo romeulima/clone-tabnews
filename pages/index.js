243750a7209d2177be2848e078a01210e4458f56
Romeu Lima <127458464+romeulima@users.noreply.github.com>
2023-11-10 18:01:06 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() { 
+    return <h1>Eae, meu advogado viado, aquele cara que tirou 900 pontos na OAB. Agora tu tem um site próprio rodando na internet. hehehe</h1>
+}
+
+export default Home;
